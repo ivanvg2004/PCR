73b838e28a820f50682221c7950a691841d90601
Iván Vargas <ivargasg@esliceu.net>
2021-04-29 10:58:59 +0200

    0.0.8

--- insert_PCR.php
+++ insert_PCR.php
@@ -5,19 +5,11 @@
   <?php require"incloudes/header.php";?>
   <form action="insert_api_PCR.php" method="GET">
   <div>
-  <label>ID</label>
-  <select name="ID">
+  <label>PCR</label>
+  <select name="PCR">
   <option value=""selected></option>
-  <option value="1">1</option>
-  <option value="2">2</option>
-  <option value="3">3</option>
-  <option value="4">4</option>
-  <option value="5">5</option>
-  <option value="6">6</option>
-  <option value="7">7</option>
-  <option value="8">8</option>
-  <option value="9">9</option>
-  <option value="10">10</option>
+  <option value="Positivo">Positivo</option>
+  <option value="Negativo">Negativo</option>
   </select>
   </div>
   <div>
