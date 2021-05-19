35bd59b44b211bc4edbc1f228fcb30e9ca1b53dc
ivan <ivargasg@esliceu.net>
2021-05-19 13:46:24 +0200

    0.1.3

--- list_PCR.php
+++ list_PCR.php
@@ -7,17 +7,17 @@
  <thead> 
   <tr>
      <th>Nom</th>
-     <th>Positiu</th>
+     <th>DNI</th>
  </tr>
 </thead>
 <tbody>
 <?php
-    $query = "SELECT * FROM COVID;
+    $query = "SELECT * FROM COVID";
     $result = mysqli_query($bbdd, $query);       
     while ($row = mysqli_fetch_assoc($result)) {
         echo "<tr>
                   <td>$row[Nom]</td>
-                  <td>$row[Positiu]</td>
+                  <td>$row[DNI_Pacient]</td>
               </tr>";
        }
       ?>
