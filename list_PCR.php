8f4f1dc948ac49429d4de3aef30f84599afb057f
ivan <ivargasg@esliceu.net>
2021-05-19 13:17:45 +0200

    0.1.1

--- list_PCR.php
+++ list_PCR.php
@@ -3,95 +3,25 @@
 <body>
   <h1>Llista de PCR fetes</h1>
   <?php require"incloudes/header.php";?>
-  <table>
+  <table> 
+ <thead> 
   <tr>
-    <td>ID</td>
-    <td>DNI</td>
-    <td>Metge</td>
-    <td>ID Sala</td>
-    <td>Data de la PCR</td>
-    <td>Positiu</td>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>DNI</td>
-    <td>Metge</td>
-    <td>A</td>
-    <td>Data de la PCR</td>
-    <td>Positiu</td>
-  </tr>
-  <tr>
-    <td>2</td>
-    <td>DNI</td>
-    <td>Metge</td>
-    <td>B</td>
-    <td>Data de la PCR</td>
-    <td>Positiu</td>
-  </tr>
-  <tr>
-    <td>3</td>
-    <td>DNI</td>
-    <td>Metge</td>
-    <td>C</td>
-    <td>Data de la PCR</td>
-    <td>Positiu</td>
-  </tr>
-  <tr>
-    <td>4</td>
-    <td>DNI</td>
-    <td>Metge</td>
-    <td>D</td>
-    <td>Data de la PCR</td>
-    <td>Positiu</td>
-  </tr>
-  <tr>
-    <td>5</td>
-    <td>DNI</td>
-    <td>Metge</td>
-    <td>E</td>
-    <td>Data de la PCR</td>
-    <td>Positiu</td>
-  </tr>
-  <tr>
-    <td>6</td>
-    <td>DNI</td>
-    <td>Metge</td>
-    <td>F</td>
-    <td>Data de la PCR</td>
-    <td>Positiu</td>
-  </tr>
-  <tr>
-    <td>7</td>
-    <td>DNI</td>
-    <td>Metge</td>
-    <td>G</td>
-    <td>Data de la PCR</td>
-    <td>Positiu</td>
-  </tr>
-  <tr>
-    <td>8</td>
-    <td>DNI</td>
-    <td>Metge</td>
-    <td>H</td>
-    <td>Data de la PCR</td>
-    <td>Positiu</td>
-  </tr>
-  <tr>
-    <td>9</td>
-    <td>DNI</td>
-    <td>Metge</td>
-    <td>I</td>
-    <td>Data de la PCR</td>
-    <td>Positiu</td>
-  </tr>
-  <tr>
-    <td>10</td>
-    <td>DNI</td>
-    <td>Metge</td>
-    <td>J</td>
-    <td>Data de la PCR</td>
-    <td>Positiu</td>
-  </tr>
-  </table>
+     <th>Nom</th>
+     <th>Positiu</th>
+ </tr>
+</thead>
+<tbody>
+<?php
+    $query = "SELECT * FROM COVID;
+    $result = mysqli_query($bbdd, $query);       
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>
+                  <td>$row[Nom]</td>
+                  <td>$row[Positiu]</td>
+              </tr>";
+       }
+      ?>
+ </tbody> 
+</table>
 </body>
 </html>
